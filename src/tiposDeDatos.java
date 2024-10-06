5cdc131f0697282421fa3bf209548192c2f68bad
Alejandro-Fernandez-Herrera <alejandro.fernandez.herrera@correounivalle.edu.co>
2024-10-06 08:17:20 -0500

    tipos de datos

---
+++ src/tiposDeDatos.java
@@ -0,0 +1,20 @@
+public class tiposDeDatos {
+
+    public static void main(String[] args) {
+        boolean booleano = true;
+        char caracter = 'a';
+        String cadena = "estoy aprendiendo con Globant University";
+        int entero = 10;
+        long largo = -9000000000000L;
+        float decimal = 1.0000000099F;
+
+        System.out.println("hola, estos son tipos de datos:");
+        System.out.println("boolean: " + booleano);
+        System.out.println("char: " + caracter);
+        System.out.println("String: " + cadena);
+        System.out.println("int: " + entero);
+        System.out.println("long: " + largo);
+        System.out.println("float: " + decimal);
+
+    }
+}
